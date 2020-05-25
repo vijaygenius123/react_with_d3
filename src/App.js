436e334e1d4b286866f1087bcf3f5934e954c976
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-25 16:36:07 +0530

    Added Add, Remove And Filter Functionality

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { select } from 'd3'
 import './App.css'
 
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 function App() {
   const svgRef = useRef();
+  const [data, setData] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
   useEffect(() => {
     const svg = select(svgRef.current);
-
     svg.selectAll('circle')
-      .data(data)
+      .data(data, d => d)
       .join(
         enter => enter
           .append('circle')
           .attr('r', d => d)
           .attr('cx', (d, i) => (i * 30) + 20)
-          .attr('cy', 240),
-        update => console.log(update),
-        exit => console.log(exit)
+          .attr('cy', 240)
+          .attr('fill', 'blue'),
+        update => {
+          update.attr('fill', 'green')
+        },
+        exit => exit.remove()
       );
-  }, [])
+  }, [data])
   return (
-    <div>
+    <div className="App">
+
       <svg ref={svgRef} height="480" width="640">
       </svg>
-    </div>
+
+      <div>
+        <button onClick={() => setData([...data, Math.random() * 10])}>Add Data</button>
+        <button onClick={() => setData([...data.slice(1)])}>Remove Data</button>
+        <button onClick={() => setData(data.filter(d => d <= 5))}>Filter Data</button>
+      </div>
+    </div >
   );
 }
 
