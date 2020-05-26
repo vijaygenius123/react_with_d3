2076a07b6d3650b703c5e4e0d28be6890937cba8
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-26 22:00:48 +0530

    Created xScale & yScale

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { select } from 'd3'
+import { select, scaleBand, scaleLinear, max } from 'd3'
 import './App.css'
 import axios from 'axios'
 const COVID_19_URL = 'https://api.covid19india.org/data.json'
@@ -11,9 +11,18 @@ function App() {
     const svg = select(svgRef.current);
 
     axios.get(COVID_19_URL).then(response => {
-      console.log(response.data)
-      setData(response.data.statewise)
+      //console.log(response.data)
+      const data = response.data.statewise;
+      console.log(data)
+      const xScale = scaleBand().range([0, 640]).domain(data.map((d, i) => i));
+      const yScale = scaleLinear().range([480, 0]).domain([0, max(data, d => d.confirmed)])
+
+      console.log(xScale(10))
+      console.log(yScale(150600))
+      svg.selectAll('rect');
+
     })
+
 
   }, [])
   return (
