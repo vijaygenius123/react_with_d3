a7781e9ed34977e35811855193df2d35f4c8bc13
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-26 21:51:52 +0530

    Setting The Data In State

--- src/App.js
+++ src/App.js
@@ -2,38 +2,25 @@ import React, { useRef, useEffect, useState } from 'react';
 import { select } from 'd3'
 import './App.css'
 import axios from 'axios'
+const COVID_19_URL = 'https://api.covid19india.org/data.json'
 function App() {
   const svgRef = useRef();
-  const [data, setData] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  const [data, setData] = useState()
 
   useEffect(() => {
     const svg = select(svgRef.current);
-    svg.selectAll('circle')
-      .data(data, (d, i) => i)
-      .join(
-        enter => enter
-          .append('circle')
-          .attr('r', d => d)
-          .attr('cx', (d, i) => (i * 30) + 20)
-          .attr('cy', 240)
-          .attr('fill', 'blue'),
-        update => {
-          update.attr('fill', 'green')
-        },
-        exit => exit.remove()
-      );
-  }, [data])
+
+    axios.get(COVID_19_URL).then(response => {
+      console.log(response.data)
+      setData(response.data.statewise)
+    })
+
+  }, [])
   return (
     <div className="App">
 
       <svg ref={svgRef} height="480" width="640">
       </svg>
-
-      <div>
-        <button onClick={() => setData([...data, Math.random() * 10])}>Add Data</button>
-        <button onClick={() => setData([...data.slice(1)])}>Remove Data</button>
-        <button onClick={() => setData(data.filter(d => d <= 5))}>Filter Data</button>
-      </div>
     </div >
   );
 }
